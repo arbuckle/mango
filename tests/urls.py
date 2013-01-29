f5f14bfde373c67d2b6ee108ce5de16295328822
David Arbuckle <dintlu@gmail.com>
2013-01-29 00:40:25 -0500

    Added URL to view all mango test templates.
    Signed-off-by: David Arbuckle <dintlu@gmail.com>

--- tests/urls.py
+++ tests/urls.py
@@ -4,5 +4,6 @@ from tests.home.views import Home
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
+    url(r'^mango-templates/$', Home.as_view(template_name="tests/home/templates/mango_templates.html"), name='mango-templates'),
 )
 urlpatterns += staticfiles_urlpatterns()
